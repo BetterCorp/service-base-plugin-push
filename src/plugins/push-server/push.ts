b2d5c22a49901c0727bb18bd53ad18d243d64eb9
Mitchell R <support@bettercorp.co.za>
2022-04-29 05:40:01 +0200

    web server updated

--- src/plugins/push-server/push.ts
+++ src/plugins/push-server/push.ts
@@ -5,13 +5,13 @@ import { PushSubscription } from 'web-push';
 export class pushServer extends CPluginClient<any> {
   public readonly _pluginName: string = "push-server";
 
-  onSubscribe(listener: (sub: subscriptionRequest) => void) {
-    this.onEvent('subscribe', listener);
+  onSubscribe(listener: { (sub: subscriptionRequest): Promise<void>; }) {
+    this.onEvent<subscriptionRequest>('subscribe', listener);
   }
-  onUnSubscribe(listener: (sub: subscriptionRequest) => void) {
-    this.onEvent('unsubscribe', listener);
+  onUnSubscribe(listener: { (sub: subscriptionRequest): Promise<void>; }) {
+    this.onEvent<subscriptionRequest>('unsubscribe', listener);
   }
-  send(subscription: PushSubscription, data: IWebPushData) {
-    return this.emitEventAndReturn('send', { subscription, data });
+  async send(subscription: PushSubscription, data: IWebPushData) {
+    return await this.emitEventAndReturn('send', { subscription, data });
   }
 }
